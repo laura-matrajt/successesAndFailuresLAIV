79e7f5ed2498b4fb223904a6e7614fc4635e8c88
laurama <matrajt@gmail.com>
2016-10-12 13:03:47 -0700

    Revert "this file has a function that will model the antigenic distance between"
    This reverts commit 931d14c7f78c00c7d8bc32606d6f3bad5d5725f9.

--- .idea/hillFunction.py
+++
@@ -1,6 +0,0 @@
-'''
-created on Sept 20th 2016
-This file will create a hill function to be the function that relates antibody efficacy and antigenic distance in my model
-'''
-
-from __future__ import
